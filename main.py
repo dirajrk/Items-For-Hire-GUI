115df25d0b845bd8b6af6bc24d1ee3f456e185a3
Diraj Ravikumar <dirajravikumar@my.jcu.edu.au>
2017-02-28 18:40:01 +0530

    Updated GitHub link.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
 Name: Diraj Ravikumar
 Student ID: 13255244
-GitHub Link: https://github.com/dirajravikumar/DirajRavikumarA2
+GitHub Link: https://github.com/dirajk/DirajRavikumarA2
 
 
 Date: 08 June 2016
